ed178c5238c69dd5f6ac5cbdeb25284cc5dbcdc3
Serena Kong <mayplesap@Serenas-Air.localdomain>
2021-10-28 17:41:08 -0700

    added post and get route for strings

---
+++ routes/strings.js
@@ -0,0 +1,46 @@
+"use strict";
+
+/** Routes for strings */
+
+const jsonschema = require("jsonschema");
+const express = require("express");
+const router = new express.Router();
+
+const { BadRequestError } = require("../expressError");
+const stringList = require("../stringList");
+
+const stringSchema = require("../schemas/stringSchema.json");
+
+
+/** POST /strings
+ * 
+ * add string { string }
+ * 
+ * returns { added: string } 
+ * 
+ */
+router.post("/", function(req, res, next) {
+  const validator = jsonschema.validate(req.body, stringSchema);
+  if (!validator.valid) {
+    const errs = validator.errors.map(e => e.stack);
+    throw new BadRequestError(errs);
+  }
+
+  const string = req.body.string;
+  stringList.push(string);
+  return res.status(201).json({ added: string });
+});
+
+/** GET /strings
+ * 
+ * get all strings form list
+ * 
+ * returns { strings }
+ *  where strings is array of strings
+ */
+router.get("/", function(req, res, next) {
+  const strings = stringList;
+  return res.json({ strings });
+});
+
+module.exports = router;
